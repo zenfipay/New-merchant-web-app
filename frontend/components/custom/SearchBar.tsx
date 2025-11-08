a24b940791d0bc2e967b4a5bf7579673d163cf17
DavidAdedigba <daveadedigba@gmail.com>
2025-11-08 09:41:42 -0800

    casing

--- frontend/components/custom/SearchBar.tsx
+++ frontend/components/custom/SearchBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function  SearchBar() {
     return (
         <div className="relative w-[205px] h-8 flex items-center gap-1 p-1.5 rounded-lg ring ring-[#EEEEEE]">
-            <Image src="/icons/searchicon.svg" alt="search icon" width={18} height={18} className="" />
+            <Image src="/icons/searchIcon.svg" alt="search icon" width={18} height={18} className="" />
             <input
                 type="text"
                 placeholder="Search via ID..."
